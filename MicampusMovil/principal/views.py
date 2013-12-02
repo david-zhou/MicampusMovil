7a15cb46ae2ae7760314285a32d4ba5ee2143c8f
Jadakra <jadakra@gmail.com>
2013-12-02 01:44:34 -0800

    Boleta y Practicas

--- MicampusMovil/principal/views.py
+++ MicampusMovil/principal/views.py
@@ -25,8 +25,10 @@ def v_LogIn(request,Matricula,Password):
 		return render_to_response("LogIn.html" ,{"Negativo":Negativo}, context_instance = RequestContext(request))
 
 def v_Boleta(request, Matricula):
-		Boleta = Grupo_Alumno.objects.raw("Select A.Nombre as NombreAlumno, A.Matricula, GA.Clave_Grupo_Alumno,M.Clave_Materia, G.Clave_Grupo, M.Nombre as NombreMateria, GA.C1, GA.C2, GA.C3, GA.Faltas_Totales, GA.Calificacion_Final FROM principal_grupo_alumno GA, principal_alumno A, principal_materia M, principal_grupo G WHERE (A.Matricula = '"'%%%%'+str(Matricula)+'%%%%'"'  ) AND M.Clave_Materia=G.Clave_Materia_id AND GA.Matricula_id = A.Matricula")
-		return render_to_response("Boleta.html" ,{"MateriaBoleta":Boleta}, context_instance = RequestContext(request))
+		NombreAlumno = Alumno.objects.raw("Select M.Nombre, GA.Clave_Grupo_Alumno, A.Matricula, A.Nombre as NombreAlumno, GA.C1 as Cal1, GA.C2 as Cal2, GA.C3 as Cal3, GA.Faltas_Totales as Faltas, GA.Calificacion_Final as Califinal FROM (principal_grupo_alumno GA INNER JOIN principal_alumno A ON  GA.Matricula_id = A.Matricula ) INNER JOIN principal_grupo G ON GA.Clave_Grupo_id = G.Clave_Grupo INNER JOIN principal_materia  M ON G.Clave_Materia_id = M.Clave_Materia WHERE Matricula = '"+Matricula+"'") 
+		Alumnops = Alumno.objects.raw("Select A.Nombre, A.Matricula FROM principal_alumno A WHERE (A.Matricula ='"+Matricula+"')") 
+		#Boleta = Alumno.objects.raw("Select A.Nombre as NombreAlumno, A.Matricula, GA.Clave_Grupo_Alumno, M.Clave_Materia, G.Clave_Grupo, M.Nombre as NombreMateria, GA.C1 as Cal1, GA.C2 as Cal2, GA.C3 as Cal3, GA.Faltas_Totales as Faltas, GA.Calificacion_Final as Califinal FROM (principal_grupo_alumno GA INNER JOIN principal_alumno A ON GA.Matricula_id = A.Matricula) INNER JOIN (principal_materia M INNER JOIN principal_grupo G ON G.Clave_Materia_id=M.Clave_Materia) ON G.Clave_Grupo=GA.Clave_Grupo_id WHERE (A.Matricula ='"+str(Matricula)+"'")
+		return render_to_response("Boleta.html" ,{"NombreAlumno":NombreAlumno,"Alumnops":Alumnops}, context_instance = RequestContext(request))
 
 def v_Practicas(request, Matricula):
 	return render_to_response("Practicas.html", {"Matricula":Matricula}, context_instance = RequestContext(request))

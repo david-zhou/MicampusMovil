b847ff9579eb4fbb1a608fb7a9be884bef3ab830
Jadakra <jadakra@gmail.com>
2013-12-11 20:38:29 -0800

    Arreglados los backs
    botones verga
    les deje el tamaño igual.

--- MicampusMovil/principal/views.py
+++ MicampusMovil/principal/views.py
@@ -28,7 +28,7 @@ def v_Boleta(request, Matricula):
 		NombreAlumno = Alumno.objects.raw("Select M.Nombre, GA.Clave_Grupo_Alumno, A.Matricula, A.Nombre as NombreAlumno, GA.C1 as Cal1, GA.C2 as Cal2, GA.C3 as Cal3, GA.Faltas_Totales as Faltas, GA.Calificacion_Final as Califinal FROM (principal_grupo_alumno GA INNER JOIN principal_alumno A ON  GA.Matricula_id = A.Matricula ) INNER JOIN principal_grupo G ON GA.Clave_Grupo_id = G.Clave_Grupo INNER JOIN principal_materia  M ON G.Clave_Materia_id = M.Clave_Materia WHERE Matricula = '"+Matricula+"'") 
 		Alumnops = Alumno.objects.raw("Select A.Nombre, A.Matricula FROM principal_alumno A WHERE (A.Matricula ='"+Matricula+"')") 
 		#Boleta = Alumno.objects.raw("Select A.Nombre as NombreAlumno, A.Matricula, GA.Clave_Grupo_Alumno, M.Clave_Materia, G.Clave_Grupo, M.Nombre as NombreMateria, GA.C1 as Cal1, GA.C2 as Cal2, GA.C3 as Cal3, GA.Faltas_Totales as Faltas, GA.Calificacion_Final as Califinal FROM (principal_grupo_alumno GA INNER JOIN principal_alumno A ON GA.Matricula_id = A.Matricula) INNER JOIN (principal_materia M INNER JOIN principal_grupo G ON G.Clave_Materia_id=M.Clave_Materia) ON G.Clave_Grupo=GA.Clave_Grupo_id WHERE (A.Matricula ='"+str(Matricula)+"'")
-		return render_to_response("Boleta.html" ,{"NombreAlumno":NombreAlumno,"Alumnops":Alumnops}, context_instance = RequestContext(request))
+		return render_to_response("Boleta.html" ,{"Matricula":Matricula,"NombreAlumno":NombreAlumno,"Alumnops":Alumnops}, context_instance = RequestContext(request))
 
 def v_Practicas(request, Matricula):
 	return render_to_response("Practicas.html", {"Matricula":Matricula}, context_instance = RequestContext(request))
@@ -49,7 +49,7 @@ def v_Historial_Academico(request, Matricula):
 	Octavo = Materia.objects.raw("Select M.Clave_Materia, M.Nombre, GA.Calificacion_Final FROM (((principal_alumno AS A inner join principal_plan_de_estudios_materia AS PM ON A.Clave_Plan_De_Estudios_id = PM.Clave_Plan_De_Estudios_id )inner join principal_materia AS M ON M.Clave_Materia = PM.Clave_Materia_id) inner join principal_grupo AS G ON G.Clave_Materia_id = M.Clave_Materia   )left join principal_grupo_alumno AS GA ON GA.Clave_Grupo_id = G.Clave_Grupo WHERE A.Matricula = '"+Matricula+"' AND PM.Semestre = '8'")
 	A = Alumno.objects.raw("Select * FROM principal_alumno AS A WHERE A.Matricula = '25697'")
 	
-	return render_to_response("Historial_Academico.html" ,{"Historial":Historial, "Segundo":Segundo, "Tercero":Tercero,"Cuarto":Cuarto,"Quinto":Quinto,"Sexto":Sexto,"Septimo":Septimo,"Octavo":Octavo, "A":A}, context_instance = RequestContext(request))
+	return render_to_response("Historial_Academico.html" ,{"Matricula":Matricula,"Historial":Historial, "Segundo":Segundo, "Tercero":Tercero,"Cuarto":Cuarto,"Quinto":Quinto,"Sexto":Sexto,"Septimo":Septimo,"Octavo":Octavo, "A":A}, context_instance = RequestContext(request))
 
 
 def v_Servicio_Social_Requisitos(request, Matricula):
